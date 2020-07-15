57175de965204405e0e292aeeffa2b30b885c8c7
renan escudeiro <renanescudeiro1@gmail.com>
2020-07-15 17:37:54 -0300

    Model 4, 5 - finalizado

--- Models/Noticia.cs
+++ Models/Noticia.cs
@@ -7,13 +7,6 @@ namespace Eplayers.Models {
 
     public class Noticia : EplayersBase, INoticia {
 
-        public Noticia (int idNoticia, string titulo, string texto, string imagem) {
-            this.IdNoticia = idNoticia;
-            this.Titulo = titulo;
-            this.Texto = texto;
-            this.Imagem = imagem;
-
-        }
         public int IdNoticia { get; set; }
         public string Titulo { get; set; }
         public string Texto { get; set; }
@@ -47,7 +40,7 @@ namespace Eplayers.Models {
                 noticia.IdNoticia = Int32.Parse (linha[0]);
                 noticia.Titulo = linha[1];
                 noticia.Texto = linha[2];
-                noticia.Imagem = linha[2];
+                noticia.Imagem = linha[3];
 
                 noticias.Add (noticia);
             }
